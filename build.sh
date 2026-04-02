3045f1a746b5d1cea9c021aed72795e93240fd21
tejakavitapu <tejakavitapu@gmail.com>
2026-04-02 12:28:01 +0530

    4th

--- build.sh
+++ build.sh
@@ -2,13 +2,15 @@
 # Exit on error
 set -o errexit
 
+# Verify Python version
+python3 --version
+
 # Install dependencies
 pip install -r requirements.txt
 
 # Run migrations
-python manage.py makemigrations
-python manage.py migrate
+python3 manage.py makemigrations
+python3 manage.py migrate
 
 # Collect static files
-# Make sure whitenoise is configured in settings.py for this to work
-python manage.py collectstatic --no-input
+python3 manage.py collectstatic --no-input
